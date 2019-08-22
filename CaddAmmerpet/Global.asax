3aad57ae2df3ae32ac1b93293183ae56ba235ea9
Prakash Duthallour <prakash.duthallour@letmecall.com>
2019-08-22 22:34:47 +0530

    first commit

---
+++ CaddAmmerpet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CaddAmmerpet.MvcApplication" Language="C#" %>
---
+++ CaddAmmerpet/Models/StuInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CaddAmmerpet.Models
+{
+    public class StuInfo
+    {
+        [Display(Name ="Name",Order =1)]
+        public string NameLabel { get; set; }
+
+        public string StuName { get; set; }
+        [Display(Name = "Mobile", Order = 1)]
+        public string MobileLabel { get; set; }
+
+        public string Mobile { get; set; }
+
+
+    }
+}
